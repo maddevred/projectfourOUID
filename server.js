3e2051c25ecf1f6294a28d5f2fc570c7532cd1d6
Amari James <mar.james1998@gmail.com>
2021-01-25 12:22:46 -0500

    uptodate

--- server.js
+++ server.js
@@ -1,51 +1,51 @@
-// require('dotenv').config();
-// const axios = require('axios');
-// const express = require('express');
-// const layouts = require('express-ejs-layouts');
-// const session = require('express-session');
-// const passport = require('./config/ppConfig');
-// const flash = require('connect-flash');
-// const SECRET_SESSION = process.env.SECRET_SESSION;
-// const app = express();
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const SECRET_SESSION = process.env.SECRET_SESSION;
+const app = express();
 
-// const isLoggedIn = require('./middleware/isLoggedIn');
-// const db = require('./models');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const db = require('./models');
 
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
-// app.use(require('morgan')('dev'));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(__dirname + '/public'));
-// app.use(layouts);
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(layouts);
 
-// const sessionObject = {
-//   secret: SECRET_SESSION,
-//   resave: false,
-//   saveUninitialized: true
-// }
+const sessionObject = {
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}
 
-// app.use(session(sessionObject));
+app.use(session(sessionObject));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use(flash());
+app.use(flash());
 
-// app.use((req, res, next) => {
-//   res.locals.alerts = req.flash();
-//   res.locals.currentUser = req.user;
-//   next();
-// });
+app.use((req, res, next) => {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
-// app.get('/profile', isLoggedIn, (req, res) => {
-//   res.render('profile');
-// });
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
 
-// app.use('/auth', require('./routes/auth'));
+app.use('/auth', require('./routes/auth'));
 
-// const PORT = process.env.PORT || 3000;
-// const server = app.listen(PORT, () => {
-//   console.log(`🎧 Your server is now running! Open local host ${PORT} to view! 🎧`);
-// });
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`🎧 Your server is now running! Open local host ${PORT} to view! 🎧`);
+});
 
-// module.exports = server;
+module.exports = server;
