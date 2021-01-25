67e00243a29803627f7061ec2fd0796832940ad6
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2021-01-25 09:39:56 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const layouts = require('express-ejs-layouts');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(require('morgan')('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use(layouts);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+app.use('/auth', require('./routes/auth'));
+
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
+});
+
+module.exports = server;
