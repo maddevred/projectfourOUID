c3d6566c3f42d85229deb13ee128cdabf25c5f8d
Amari James <mar.james1998@gmail.com>
2021-01-25 12:12:06 -0500

    fixederror

--- models/user.js
+++ models/user.js
@@ -1,69 +1,69 @@
-'use strict';
-const bcrypt = require('bcrypt'); // making a hash password for the user
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    static associate(models) {
-      // define association here
-    }
-  };
-  user.init({
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        len: {
-          args: [1,99],
-          msg: 'Name must be between 1 and 99 characters'
-        }
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: {
-          msg: 'Invalid email'
-        }
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        len: {
-          args: [8,99],
-          msg: 'Password must be between 8 and 99 characters'
-        }
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+// 'use strict';
+// const bcrypt = require('bcrypt'); // making a hash password for the user
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class user extends Model {
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   user.init({
+//     name: {
+//       type: DataTypes.STRING,
+//       validate: {
+//         len: {
+//           args: [1,99],
+//           msg: 'Name must be between 1 and 99 characters'
+//         }
+//       }
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       validate: {
+//         isEmail: {
+//           msg: 'Invalid email'
+//         }
+//       }
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       validate: {
+//         len: {
+//           args: [8,99],
+//           msg: 'Password must be between 8 and 99 characters'
+//         }
+//       }
+//     }
+//   }, {
+//     sequelize,
+//     modelName: 'user',
+//   });
 
-  user.addHook('beforeCreate', function(pendingUser) {
-    // bcrypt hash a password for us
-    let hash = bcrypt.hashSync(pendingUser.password, 12);
-    // set password to equal the hash
-    pendingUser.password = hash;
-    console.log(pendingUser);
-  });
+//   user.addHook('beforeCreate', function(pendingUser) {
+//     // bcrypt hash a password for us
+//     let hash = bcrypt.hashSync(pendingUser.password, 12);
+//     // set password to equal the hash
+//     pendingUser.password = hash;
+//     console.log(pendingUser);
+//   });
   
-  user.prototype.validPassword = function(passwordTyped) {
-    let correctPassword = bcrypt.compareSync(passwordTyped, this.password);
-    console.log('Inside of validPassword', correctPassword);
-    // return true or false based on correct password or not
-    return correctPassword;
-  }
+//   user.prototype.validPassword = function(passwordTyped) {
+//     let correctPassword = bcrypt.compareSync(passwordTyped, this.password);
+//     console.log('Inside of validPassword', correctPassword);
+//     // return true or false based on correct password or not
+//     return correctPassword;
+//   }
   
-  // remove the password before it gets serialized 
-  user.prototype.toJSON = function() {
-    console.log('Inside of the toJSON method');
-    let userData = this.get();
-    delete userData.password;
-    console.log(userData);
-    return userData;
-  }
+//   // remove the password before it gets serialized 
+//   user.prototype.toJSON = function() {
+//     console.log('Inside of the toJSON method');
+//     let userData = this.get();
+//     delete userData.password;
+//     console.log(userData);
+//     return userData;
+//   }
 
-  return user;
-};
+//   return user;
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,9 @@
 var express = require('express');
-var passport = require('passport');
-var Account = require('../models/account');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
- res.render('index', { user : req.user });
-});
-
-router.route('/register')
-  .get(function(req, res) {
-    res.render('register', { });
-  })
-  .post(function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-      if (err) {
-        return res.render('register', {info: 'Sorry. That username already exists. Try again.'});
-      }
-      passport.authenticate('local')(req, res, function() {
-        res.redirect('/');
-      });
-    });
-  });
-
-router.route('/login')
-  .get(function(req, res) {
-    res.render('login', { user: req.user });
-  })
-  .post(passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
-  });
-
-router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
